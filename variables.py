c68da7c6c50b4d1391447b8502590db5b3efc3a4
abimaelreyes <reyesabima@gmail.com>
2022-04-27 15:06:10 -0300

    second commit

---
+++ variables.py
@@ -0,0 +1,13 @@
+'''
+saludo = "Hola "
+nombre = input("Ingresar su nombre: ")
+
+print(saludo + nombre)
+'''
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
