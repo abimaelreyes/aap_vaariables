9a00c99682762ecf5f1116f71d76efc52cde0d14
abimaelreyes <reyesabima@gmail.com>
2022-04-30 13:37:42 -0300

    ejercicio de func

--- main.py
+++ main.py
@@ -1,100 +1,35 @@
 '''
 Abimael Reyes
-video 11 - diccionarios
+video 12 - funciones
 '''
 
-# mi_dic={"nombre":"Abi", "edad":28 , "verdad": True}
+def my_func(nombre):
+	print(f"soy {nombre}")
 
-## Obtener una key
-# print(mi_dic.get("ape", "Key no valida"))
+# my_func("Abi")
 
-## Solo muestra las key del dicionario
-# for value in mi_dic:
-# 	print(value)
+def sumar(a,b):
+	res= a + b
+	return "soy una suma", res
 
-## cambiar el value de una key
-# mi_dic["verdad"]=False
-
-## Consultar por key y value de un disccionario
-# for key, value in mi_dic.items():
-# 	print(key)
-# 	print(value)
-
-## Consultar si existe una key en el diccionario
-# if "edad" in mi_dic:
-# 	print("Hay edad")
-
-# if "apellido" in mi_dic:
-# 	print("Hay apellido")
-# else:
-# 	print("No hay apellido") 
-
-# # Agregar elementos a un dic
-
-# mi_dic["apellido"]="reyes"
-# print(mi_dic)
+tipo, resultado = sumar(3,4)
+# print(tipo)
+# print(resultado)
 
 import random
-import nombres
 
-# noms=nombres.get_nombres(2)
+def get_stats():
+	dados = [random.randint(1,6) for i in range(4)]
+	dados.sort()
+	max_dados=dados[-3:]
+	return sum(max_dados)
 
-# alumnos = {}
+stats={
+	"str": get_stats(),
+	"des": get_stats(),
+	"int": get_stats(),
+	"wis": get_stats(),
+	"cons": get_stats()
+}
 
-# for nombre in noms:
-# 	notas = [random.randint(1,10) for i in range(3)]
-# 	alumnos[nombre]= notas
-
-
-# promedios = {}
-# for nom in alumnos:
-# 	notas=alumnos[nom]
-# 	suma = 0
-# 	for nota in notas:
-# 		suma = nota + suma
-# 	promedios[nom] = suma/3
-
-# print(promedios)
-
-
-
-cant_alum=int(input("Ingrese la cant de alumnos: "))
-
-cant_notas= int(input("Ingrese la cant. de notas: "))
-
-alum=nombres.get_nombres(cant_alum)
-
-alumnos = {}
-
-# alum=[(i+1) for i in range(cant_alum)]
-
-for nombre in alum:
-	notas= [random.randint(1,10) for i in range(cant_notas)]
-	alumnos[nombre]=notas
-
-promedios ={}
-
-for nom in alumnos:
-	notas=alumnos[nom]
-	suma=0
-	for nota in notas:
-		suma=nota+suma
-	promedios[nom]=int(round(suma/cant_notas,0))
-else:
-	print("Este es el resultado: ")
-
-print(promedios)
-
-aprobados={}
-
-for nom in promedios:
-	prom=promedios[nom]
-	if prom >=6:
-		aprobado="aprobado"
-	else:
-		aprobado="reprobado"
-	aprobados[nom]=aprobado
-else:
-	print("listado de alumnos aprobados: ")
-
-print(aprobados)
+print(stats)
